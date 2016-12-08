a938cdb18bdf3fe97d0d5809422a1efdb1c5f60e
Priscille De Dumast <pdedumast@gmail.com>
2016-12-08 11:35:44 -0500

    ENH: Add epochs during network training

--- condylesClassification.py
+++ condylesClassification.py
@@ -285,6 +285,8 @@ with graph.as_default():
 # num_steps = 1001
 num_steps = 501
 
+num_epochs = 3
+
 print ""
 print "nbGroups = " + str(nbLabels)
 print "nb_hidden_layers = " + str(nb_hidden_layers)
@@ -295,22 +297,23 @@ print ""
 with tf.Session(graph=graph) as session:
 	tf.initialize_all_variables().run()
 	print("Initialized")
-	for step in range(num_steps):
-		# Pick an offset within the training data, which has been randomized.
-		# Note: we could use better randomization across epochs.
-		offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
-		# Generate a minibatch.
-		batch_data = train_dataset[offset:(offset + batch_size), :]
-		batch_labels = train_labels[offset:(offset + batch_size), :]
-		# Prepare a dictionary telling the session where to feed the minibatch.
-		# The key of the dictionary is the placeholder node of the graph to be fed,
-		# and the value is the numpy array to feed to it.
-		feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
-		_, l, predictions = session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
-		if (step % 500 == 0):
-			print("Minibatch loss at step %d: %f" % (step, l))
-			print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels)[0])
-			print("Validation accuracy: %.1f%%" % accuracy(valid_prediction.eval(), valid_labels)[0])
+	for epochs in range(0, num_epochs):
+		for step in range(num_steps):
+			# Pick an offset within the training data, which has been randomized.
+			# Note: we could use better randomization across epochs.
+			offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+			# Generate a minibatch.
+			batch_data = train_dataset[offset:(offset + batch_size), :]
+			batch_labels = train_labels[offset:(offset + batch_size), :]
+			# Prepare a dictionary telling the session where to feed the minibatch.
+			# The key of the dictionary is the placeholder node of the graph to be fed,
+			# and the value is the numpy array to feed to it.
+			feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+			_, l, predictions = session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
+			if (step % 500 == 0):
+				print("Minibatch loss at step %d: %f" % (step, l))
+				print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels)[0])
+				print("Validation accuracy: %.1f%%" % accuracy(valid_prediction.eval(), valid_labels)[0])
 	finalaccuracy = accuracy(test_prediction.eval(), test_labels)
 	print("Test accuracy: %.1f%%" % finalaccuracy[0])
 	print("\n\nConfusion matrix :")
