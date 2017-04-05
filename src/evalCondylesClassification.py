59094d46f7c554750ea8bc68ef28c131b32ba0e9
Priscille De Dumast <pdedumast@gmail.com>
2017-04-05 18:48:41 -0400

    ENH: Classify a shape, from any classifier network given in a zip file

--- src/evalCondylesClassification.py
+++ src/evalCondylesClassification.py
@@ -1,31 +1,15 @@
 import os
 import sys
 from six.moves import cPickle as pickle
-import neuralnetwork as nn
-import inputdata
+import neuralNetwork as nn
+# import inputData
 import numpy as np
 import tensorflow as tf
 
 import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-inputFile', action='store', dest='inputFile', help='Input file to classify', 
-                    default = "/Users/prisgdd/Documents/Projects/CNN/DataPriscille/surfSPHARM/5Groups-Feat/G00/34551376_Left_pp_surfSPHARM-Features.vtk")
-
-parser.add_argument('-saveModelPath', action='store', dest='saveModelPath', help='Path to the saved model to use', default='./weights_5Groups')
-
-args = parser.parse_args()
-inputFile = args.inputFile
-saveModelPath = args.saveModelPath
-
-
-# ----------------------------------------------------------------------------- #
-# 									PROGRAM										#
-# ----------------------------------------------------------------------------- #
-if nn.NUM_HIDDEN_LAYERS == 1:
-    nb_hidden_nodes_1 = 2048
-elif nn.NUM_HIDDEN_LAYERS == 2:
-    nb_hidden_nodes_1, nb_hidden_nodes_2 = 2048, 2048
+import zipfile
+import shutil
+import json
 
 
 
@@ -37,55 +21,20 @@ elif nn.NUM_HIDDEN_LAYERS == 2:
 # ----------------------------------------------------------------------------- #
 # 
 
-
-# vtk_filenames = os.listdir(inputDir)      # Juste le nom du vtk file
-
-# # Delete .DS_Store file if there is one
-# if vtk_filenames.count(".DS_Store"):
-#     vtk_filenames.remove(".DS_Store")
-
-# for shape in vtk_filenames:
-
-#     inputFile = os.path.join(inputDir,shape)
+## Reformat into a shape that's more adapted to the models we're going to train:
+#   - data as a flat matrix
+#   - labels as float 1-hot encodings
+def reformat_data(dataset, classifier):
+    dataset = dataset.reshape((-1, classifier.NUM_POINTS * classifier.NUM_FEATURES)).astype(np.float32)
+    return dataset
 
 
-
-
-def get_input(inputFile):
+def get_input_shape(data, classifier):
     # Get features in a matrix (NUM_FEATURES x NUM_POINTS)
-    data = inputdata.load_features(inputFile)
-    data = data.reshape((-1, inputdata.NUM_POINTS)).astype(np.float32)
-    data = inputdata.reformat_data(data)
+    # data = input_Data.load_features(inputFile)
+    data = data.reshape((-1, classifier.NUM_POINTS * classifier.NUM_FEATURES)).astype(np.float32)
+    data = reformat_data(data, classifier)
     return data
-
-
-
-def run_eval(data):
-    # Construct the graph
-    graph = tf.Graph()
-    with graph.as_default():
-        # Input data.
-        with tf.name_scope('Inputs_management'):
-            tf_data = tf.constant(data, name="data")
-
-        with tf.name_scope('Bias_and_weights_management'):
-            weightsDict = nn.bias_weights_creation(nb_hidden_nodes_1, nb_hidden_nodes_2)    
-        # Training computation.
-        with tf.name_scope('Network_computations'):
-            logits,weightsDict = nn.model(tf_data, weightsDict)
-            
-        with tf.name_scope('Predictions'):
-            test_prediction = tf.nn.softmax(logits)
-
-        with tf.name_scope('Restore_weights'):
-            saver = tf.train.Saver(weightsDict)
-
-    	with tf.Session(graph=graph) as session:
-    		saver.restore(session, saveModelPath)
-    		feed_dict = {tf_valid_dataset: data}
-    		prediction = session.run(test_prediction, feed_dict=feed_dict)
-    return prediction
-
 
 # ----------------- #
 # ---- RESULTS ---- #
@@ -96,27 +45,103 @@ def get_result(prediction):
 
 def main(_):
 
-    # Create session, and import existing graph
-    myData = get_input(inputFile)
-    session = tf.InteractiveSession()
-    new_saver = tf.train.import_meta_graph('./weights_5Groups.meta')
-    new_saver.restore(session, './weights_5Groups')
-    graph = tf.Graph().as_default()
 
-    # listTensor =[tensor.name for tensor in tf.get_default_graph().as_graph_def().node]
-    
-    # Get useful tensor in the graph
-    # tf_data = session.graph.get_collection(name="input:0", scope="Inputs_management") 
-    tf_data = session.graph.get_tensor_by_name("Inputs_management/input:0")
-    data_pred = session.graph.get_tensor_by_name("Predictions/output:0")
+    # Get the arguments from the command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-inputZip', action='store', dest='inputZip', help='Input zip file which contains the datasets & the parameters for the classifier', 
+                        default = "")
 
-    feed_dict = {tf_data: myData}
-    data_pred = session.run(data_pred, feed_dict=feed_dict)
+    # parser.add_argument('-inputFile', action='store', dest='inputFile', help='Input file to classify', default = "")
+
+    args = parser.parse_args()
+
+    inputZip = args.inputZip
+    # inputFile = args.inputFile
+
+    basedir = os.path.dirname(inputZip)
+    nameDir = os.path.splitext(os.path.basename(inputZip))[0]
+
+    networkDir = os.path.join(basedir, nameDir)
+    print "networkDir : " + networkDir
+
+    if os.path.isdir(networkDir):
+        shutil.rmtree(networkDir)
+    os.mkdir(networkDir) 
+
+    # Unpack archive
+    with zipfile.ZipFile(inputZip) as zf:
+        zf.extractall(basedir)
+
+    jsonFile = os.path.join(networkDir, 'classifierInfo.json')
+    saveModelPath = os.path.join(networkDir, 'CondylesClassifier')
+    pickleToClassify = os.path.join(networkDir, 'toClassify.pickle')
+    #
+    # Create a network for the classification
+    #
+    with open(jsonFile) as f:    
+        jsonDict = json.load(f)
+
+
+    # In case our JSON file doesnt contain a valid Classifier
+    if not jsonDict.has_key('CondylesClassifier'):
+        print "Error: Couldn't parameterize the network."
+        print "There is no 'CondylesClassifier' model."
+        return 0
+
+
+    # If we have the Classifier, set all parameters for the network
+    classifier = nn.neuralNetwork()
+
+    # Essential parameters
+    if 'NUM_CLASSES' in jsonDict['CondylesClassifier']:
+        classifier.NUM_CLASSES = jsonDict['CondylesClassifier']['NUM_CLASSES'] 
+    else:
+        print "Missing NUM_CLASSES"
     
-    result = get_result(data_pred)
-    print "Shape : " + os.path.basename(inputFile)
-    print "Group predicted :" + str(result) + "\n"
+    if 'NUM_POINTS' in jsonDict['CondylesClassifier']:
+        classifier.NUM_POINTS = jsonDict['CondylesClassifier']['NUM_POINTS']
+    else:
+        print "Missing NUM_POINTS"
+
+    if 'NUM_FEATURES' in jsonDict['CondylesClassifier']:
+        classifier.NUM_FEATURES = jsonDict['CondylesClassifier']['NUM_FEATURES']
+    else:
+        print "Missing NUM_FEATURES"
+
+
+    favorite_color = pickle.load( open( pickleToClassify, "rb" ) )
+
+    print favorite_color
+
+    print " .......... \n"
+    for file in favorite_color.keys():
+        print file 
+
+        print "\n\n FINIIII \n\n"
+
+        # Create session, and import existing graph
+        # print shape
+        myData = get_input_shape(favorite_color[file], classifier)
+        session = tf.InteractiveSession()
+
+
+        new_saver = tf.train.import_meta_graph(saveModelPath + '.meta')
+        new_saver.restore(session, saveModelPath)
+        graph = tf.Graph().as_default()
+
+        # Get useful tensor in the graph
+        tf_data = session.graph.get_tensor_by_name("Inputs_management/input:0")
+        data_pred = session.graph.get_tensor_by_name("Predictions/output:0")
+
+        feed_dict = {tf_data: myData}
+        data_pred = session.run(data_pred, feed_dict=feed_dict)
+
+        result = get_result(data_pred)
+        print "Shape : " + os.path.basename(file)
+        print "Group predicted :" + str(result) + "\n"
+
     return result
+
 
 
 if __name__ == '__main__':
